92d475a3e0a1134385e5176038a46d8f39598df4
卫慧杰 <w920098695@sina.cn>
2020-02-13 01:04:08 +0800

    视频全屏实现

--- video/js/index.js
+++ video/js/index.js
@@ -1,7 +1,9 @@
 var videoObj = document.querySelector('video'); // video
 var expand = document.querySelector('.video-expand');
 // play()
-
+videoObj.addEventListener('click', function(){
+  videoObj.play()
+})
 expand.addEventListener('click', function() {
   videoObj.webkitRequestFullScreen()
 })
